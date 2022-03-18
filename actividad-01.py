7f4a96b225a9603fe3126d86d398dca334f01650
Nicolas Vasquez <negitausen.1999@gmail.com>
2022-03-17 23:10:17 -0300

    Se adjunta actividad-01

---
+++ actividad-01.py
@@ -0,0 +1,127 @@
+import turtle as tw
+
+# configuramos el mundo de tortugas
+#tw.bgpic( "Grid 800x600.png" )
+screen = tw.Screen()
+screen.setup( 800, 600 )
+screen.title( "Actividad 01" )
+
+# creamos una tortuga
+tortuga = tw.Turtle()
+tortuga.shape( 'arrow' )
+tortuga.setheading( 90 )
+tortuga.speed( 4 )
+
+# la preparamos para dibujar
+tortuga.pensize( 1 )
+tortuga.pencolor( "#F60A23" )
+
+# dibujamos
+tortuga.penup()
+tortuga.goto( 0, 0 )
+tortuga.pendown()
+
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.right( 90 )
+tortuga.forward( 100 )
+tortuga.penup()
+#tw.bgpic( "Grid 800x600.png" )
+screen = tw.Screen()
+screen.setup( 800, 600 )
+screen.title( "Actividad 01" )
+
+# creamos una tortuga
+tortuga = tw.Turtle()
+tortuga.shape( 'arrow' )
+tortuga.setheading( 90 )
+tortuga.speed( 4 )
+
+# la preparamos para dibujar
+tortuga.pensize( 1 )
+tortuga.pencolor( "#F60A23" )
+
+# dibujamos
+tortuga.penup()
+tortuga.goto( 200, 200 )
+tortuga.pendown()
+tortuga.circle(50)
+
+#tw.bgpic( "Grid 800x600.png" )
+screen = tw.Screen()
+screen.setup( 800, 600 )
+screen.title( "Actividad 01" )
+
+# creamos una tortuga
+tortuga = tw.Turtle()
+tortuga.shape( 'turtle' )
+tortuga.setheading( 90 )
+tortuga.speed( 4 )
+
+# la preparamos para dibujar
+tortuga.pensize( 1 )
+tortuga.pencolor( "#1AAD00" )
+
+# dibujamos
+tortuga.penup()
+tortuga.goto( -150, 150 )
+tortuga.pendown()
+
+tortuga.forward(80)
+tortuga.right(90)
+tortuga.forward(40)
+tortuga.right(90)
+tortuga.forward(80)
+tortuga.right(90)
+tortuga.forward(40)
+tortuga.forward(80)
+tortuga.right(90)
+tortuga.forward(40)
+tortuga.right(90)
+tortuga.forward(80)
+tortuga.right(90)
+tortuga.forward(40)
+tortuga.forward(80)
+tortuga.right(90)
+tortuga.forward(40)
+tortuga.right(90)
+tortuga.forward(80)
+tortuga.right(90)
+tortuga.forward(40)
+tortuga.forward(80)
+tortuga.right(90)
+tortuga.forward(40)
+tortuga.right(90)
+tortuga.forward(80)
+tortuga.right(90)
+tortuga.forward(40)
+tortuga.penup()
+
+
+
+# eso es todo
+tw.done()
